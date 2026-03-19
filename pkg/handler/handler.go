11e5cf2712a2a1d73507b2420e51b993a39c5d14
lux <119802379+ashhoxx@users.noreply.github.com>
2026-03-19 12:48:53 +0300

    create userIdentity

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -22,7 +22,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		auth.POST("sign-in", h.signIp)
 	}
 
-	api := router.Group("/api")
+	api := router.Group("/api", h.userIdentity)
 	{
 		lists := api.Group("/lists")
 		{
